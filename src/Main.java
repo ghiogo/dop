23e0e094b9e01f12f8dc2c2bc27ad8e93de9fa41
ghiogo <erkinovakami09@gmail.com>
2022-10-25 17:56:10 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "kamila";
+        int age=14;
+        final String namePerson="Kamila";
+        age = 15;
+        
+    }
+    private static void learn (int a, int b){
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
